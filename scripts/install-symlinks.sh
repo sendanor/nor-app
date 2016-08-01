41611e746aec2c8679c0f8ccc0dc9b967e76b346
Jaakko-Heikki Heusala <jhh@sendanor.com>
2016-08-01 16:26:14 +0000

    Removed symbolic links

--- scripts/install-symlinks.sh
+++ scripts/install-symlinks.sh
@@ -5,24 +5,24 @@ test -d src/public/_libs
 
 cd src/public/_libs
 
-test ! -h ace                 && ln -s ../../../node_modules/ace-builds/src-min ace
-test ! -h angular             && ln -s ../../../node_modules/angular angular
-test ! -h angular-datatables  && ln -s ../../../node_modules/angular-datatables/dist/ angular-datatables
-test ! -h angular-route       && ln -s ../../../node_modules/angular-route angular-route
-test ! -h angular-sanitize    && ln -s ../../../node_modules/angular-sanitize angular-sanitize
-test ! -h angular-ui-router   && ln -s ../../../node_modules/angular-ui-router/release angular-ui-router
-test ! -h bootstrap           && ln -s ../../../node_modules/bootstrap/dist bootstrap
-test ! -h font-awesome        && ln -s ../../../node_modules/font-awesome/ font-awesome
-test ! -h jquery              && ln -s ../../../node_modules/jquery/dist/ jquery
-test ! -h ng-prettyjson       && ln -s ../../../node_modules/ng-prettyjson/dist/ ng-prettyjson
-test ! -h purecss             && ln -s ../../../node_modules/purecss/build/ purecss
-test ! -h tv4                 && ln -s ../../../node_modules/tv4/ tv4
+test ! -e ace                 && cp -afr ../../../node_modules/ace-builds/src-min ace
+test ! -e angular             && cp -afr ../../../node_modules/angular angular
+test ! -e angular-datatables  && cp -afr ../../../node_modules/angular-datatables/dist/ angular-datatables
+test ! -e angular-route       && cp -afr ../../../node_modules/angular-route angular-route
+test ! -e angular-sanitize    && cp -afr ../../../node_modules/angular-sanitize angular-sanitize
+test ! -e angular-ui-router   && cp -afr ../../../node_modules/angular-ui-router/release angular-ui-router
+test ! -e bootstrap           && cp -afr ../../../node_modules/bootstrap/dist bootstrap
+test ! -e font-awesome        && cp -afr ../../../node_modules/font-awesome/ font-awesome
+test ! -e jquery              && cp -afr ../../../node_modules/jquery/dist/ jquery
+test ! -e ng-prettyjson       && cp -afr ../../../node_modules/ng-prettyjson/dist/ ng-prettyjson
+test ! -e purecss             && cp -afr ../../../node_modules/purecss/build/ purecss
+test ! -e tv4                 && cp -afr ../../../node_modules/tv4/ tv4
 
 mkdir -p datatables.net
 cd datatables.net
 
-test ! -h css     && ln -s ../../../../node_modules/datatables.net-dt/css css
-test ! -h images  && ln -s ../../../../node_modules/datatables.net-dt/images images
-test ! -h js      && ln -s ../../../../node_modules/datatables.net/js js
+test ! -e css     && cp -afr ../../../../node_modules/datatables.net-dt/css css
+test ! -e images  && cp -afr ../../../../node_modules/datatables.net-dt/images images
+test ! -e js      && cp -afr ../../../../node_modules/datatables.net/js js
 
 exit 0
